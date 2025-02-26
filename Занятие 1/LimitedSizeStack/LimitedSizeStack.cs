26fd6b0b3fcfb9b45b2620f84dbb50f0e196fef9
Vrublevskaya1 <vrublevskaya07@list.ru>
2025-02-26 23:26:01 +0300

    lesson 1 ex1

--- Занятие 1/LimitedSizeStack/LimitedSizeStack.cs
+++ Занятие 1/LimitedSizeStack/LimitedSizeStack.cs
@@ -4,28 +4,34 @@ namespace LimitedSizeStack;
 
 public class LimitedSizeStack<T>
 {
-	public LimitedSizeStack(int undoLimit)
-	{
-	}
 
-	public void Push(T item)
-	{
-    if (items.Length == 0) return;
+    private T[] items;
+    private int top = 0;
+    private int count = 0;
+
+    public LimitedSizeStack(int undoLimit)
+    {
+        items = new T[undoLimit];
+    }
+
+    public void Push(T item)
+    {
+        if (items.Length == 0) return;
 
         items[top] = item;
         top = (top + 1) % items.Length;
 
         if (count < items.Length) count++;
-	}
+    }
 
-	public T Pop()
-	{
-    if (count == 0)
-            throw new ArgumentException("List is empty");
+    public T Pop()
+    {
+        if (count == 0)
+        throw new ArgumentException("List is empty");
 
         top = (items.Length + top - 1) % items.Length;
         count--;
         return items[top];
-	}
+    }
 
-public int Count => count;
+    public int Count => count;
