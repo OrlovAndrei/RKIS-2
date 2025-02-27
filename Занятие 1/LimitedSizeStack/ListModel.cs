988afd7a3871e1d2607fb75f1a90a6bc5076b37a
Daria Kolmuk <nross.dasha@bk.ru>
2025-02-27 21:37:54 +0300

    Lesson 1: changed ListMobel

--- Занятие 1/LimitedSizeStack/ListModel.cs
+++ Занятие 1/LimitedSizeStack/ListModel.cs
@@ -5,54 +5,73 @@ namespace LimitedSizeStack;
 
 public class ListModel<TItem>
 {
-    public enum TypeAction { AddItem, RemoveItem }
-    public List<TItem> Items { get; }
-    public int UndoLimit;, undoLimit)
+    public enum TypeAction 
+    { 
+        AddItem, RemoveItem
+    }
 
-	private LimitedSizeStack<(TypeAction action, TItem item, int index)> history { get;}
-	
-    public ListModel(List<TItem> items, int undoLimit)
-	{
-        Items = Items ?? new List<TItem>();
+    public List<TItem> Items { get; private set; }
+    public int UndoLimit { get; private set; }
+
+    private LimitedSizeStack<Tuple<TypeAction, TItem, int>> StoryAction { get; set; }
+
+    public ListModel(int undoLimit)
+    {
+        Items = new List<TItem>();
         UndoLimit = undoLimit;
-        history = new LimitedSizeStack<(TypeAction, TItem, int)>(undoLimit);
+        StoryAction = new LimitedSizeStack<Tuple<TypeAction, TItem, int>>(undoLimit);
+    }
+
+    public ListModel(List<TItem> items, int undoLimit)
+    {
+        Items = items ?? throw new ArgumentNullException(nameof(items));
+        UndoLimit = undoLimit;
+        StoryAction = new LimitedSizeStack<Tuple<TypeAction, TItem, int>>(undoLimit);
     }
 
     public void AddItem(TItem item)
     {
-        if (item == null) throw new ArgumentNullException(nameof(item));
-
-        history.Push((TypeAction.AddItem, item, Items.Count));
-        Items.Add(item)
-    }
-
-
-	public void RemoveItem(int index)
-	{
-        if (index < 0 || index >= Items.Count) throw new ArgumentOutOfRangeException(nameof(index));
-
-        history.Push((TypeAction.RemoveItem, Items[index], index));
-        Items.RemoveAt(index);
-	}
-
-	public bool CanUndo()
-	{
-        return history.Count > 0
-    }
-
-	public void Undo()
-	{
-        if (!CanUndo()) throw new InvalidOperationException("No action to undo items+");
-
-        var (action, item, index) = History.Pop();
-
-        if (action == TypeAction.AddItem)
+        if (item == null)
         {
-            Items.RemoveAt(index);
+            throw new ArgumentNullException(nameof(item));
         }
-        else
+
+        StoryAction.Push(Tuple.Create(TypeAction.AddItem, item, Items.Count));
+        Items.Add(item);
+    }
+
+    public void RemoveItem(int index)
+    {
+        if (index < 0 || index >= Items.Count)
         {
-            Items.Insert(index, item);
+            throw new IndexOutOfRangeException("Index is out of range.");
+        }
+
+        StoryAction.Push(Tuple.Create(TypeAction.RemoveItem, Items[index], index));
+        Items.RemoveAt(index);
+    }
+
+    public bool CanUndo()
+    {
+        return StoryAction.Count > 0;
+    }
+
+    public void Undo()
+    {
+        if (!CanUndo())
+        {
+            throw new InvalidOperationException("No actions to undo.");
+        }
+
+        var lastAction = StoryAction.Pop();
+        switch (lastAction.Item1)
+        {
+            case TypeAction.AddItem:
+                Items.RemoveAt(lastAction.Item3);
+                break;
+            case TypeAction.RemoveItem:
+                Items.Insert(lastAction.Item3, lastAction.Item2);
+                break;
         }
     }
 }
