be94f3675026c8320d3ebe2fdcbf446c0ab0793b
Ali <147639244+EROSION6@users.noreply.github.com>
2025-02-27 22:35:04 +0300

    List Model file

--- Занятие 1/LimitedSizeStack/ListModel.cs
+++ Занятие 1/LimitedSizeStack/ListModel.cs
@@ -1,40 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace LimitedSizeStack;
-
-public class ListModel<TItem>
+namespace LimitedSizeStack
 {
-	public List<TItem> Items { get; }
-	public int UndoLimit;
-        
-	public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
-	{
-	}
+    public class ListModel<TItem>
+    {
+        public List<TItem> Items { get; }
+        public int UndoLimit;
+        private LimitedSizeStack<Action> undoStack;
 
-	public ListModel(List<TItem> items, int undoLimit)
-	{
-		Items = items;
-		UndoLimit = undoLimit;
-	}
+        public ListModel(int undoLimit)
+        {
+            Items = new List<TItem>();
+            UndoLimit = undoLimit;
+            undoStack = new LimitedSizeStack<Action>(undoLimit);
+        }
 
-	public void AddItem(TItem item)
-	{
-		Items.Add(item);
-	}
+        public ListModel(List<TItem> items, int undoLimit)
+        {
+            Items = items;
+            UndoLimit = undoLimit;
+            undoStack = new LimitedSizeStack<Action>(undoLimit);
+        }
 
-	public void RemoveItem(int index)
-	{
-		Items.RemoveAt(index);
-	}
+        public void AddItem(TItem item)
+        {
+            Items.Add(item);
+            undoStack.Push(() =>
+            {
+                Items.RemoveAt(Items.Count - 1);
+            });
+        }
 
-	public bool CanUndo()
-	{
-		return false;
-	}
+        public void RemoveItem(int index)
+        {
+            var item = Items[index];
+            Items.RemoveAt(index);
+            undoStack.Push(() =>
+            {
+                Items.Insert(index, item);
+            });
+        }
 
-	public void Undo()
-	{
-		throw new NotImplementedException();
-	}
+        public bool CanUndo()
+        {
+            if (undoStack.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Undo()
+        {
+            if (CanUndo())
+            {
+                var action = undoStack.Pop();
+                action();
+            }
+        }
+    }
 }
