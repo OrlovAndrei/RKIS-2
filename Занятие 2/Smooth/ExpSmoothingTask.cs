9315906a5c36e369397abcf2fbac3461d876984d
Vrublevskaya1 <vrublevskaya07@list.ru>
2025-03-01 13:26:35 +0300

    lesson 2: expsmoothingtask

--- Занятие 2/Smooth/ExpSmoothingTask.cs
+++ Занятие 2/Smooth/ExpSmoothingTask.cs
@@ -1,12 +1,16 @@
 using System.Collections.Generic;
 
-namespace yield;
-
+namespace yield
+{
 public static class ExpSmoothingTask
 {
 	public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
 	{
-		//Fix me!
-		return data;
+            var e = data.GetEnumerator();
+            double middle;
+
+            if (e.MoveNext())
+            {
+                middle = e.Current.ExpSmoothedY = e.Current.OriginalY;
 	}
 }
