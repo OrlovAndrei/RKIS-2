cee638c431d5bfebf252ce23669eeb9ea332fd8e
Nameless-gg <igor01.12.2004@mail.ru>
2025-03-05 23:28:19 +0300

    lesson 2: make task1

--- Занятие 2/Smooth/ExpSmoothingTask.cs
+++ Занятие 2/Smooth/ExpSmoothingTask.cs
@@ -6,7 +6,15 @@ public static class ExpSmoothingTask
 {
 	public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
 	{
-		//Fix me!
-		return data;
+		double smoothedValue = null;
+
+		foreach (point in data){
+			if(smoothedValue == null){
+				smoothedValue = point.OriginalY; //Если значение первое, сглаживание равно значению
+			}else{
+				smoothedValue = alpha * point.OriginalY + (1 - alpha) * smoothedValue; //иначе применяем формулу экспонинциального сглаживания
+			}
+			yield return point.WithExpSmoothedY(smoothedValue.Value); //возвращаем точку со сглаживанием
+		}
 	}
 }
