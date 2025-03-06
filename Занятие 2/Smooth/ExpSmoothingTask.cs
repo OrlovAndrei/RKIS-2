515853c7a94ea8b308d06551d6647efafd0421eb
Agni <63151335+crash36663@users.noreply.github.com>
2025-03-06 12:15:32 +0300

    Update ExpSmoothingTask.cs

--- Занятие 2/Smooth/ExpSmoothingTask.cs
+++ Занятие 2/Smooth/ExpSmoothingTask.cs
@@ -1,12 +1,43 @@
 using System.Collections.Generic;
 
-namespace yield;
-
-public static class ExpSmoothingTask
+namespace yield
 {
-	public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
-	{
-		//Fix me!
-		return data;
-	}
+    public static class ExpSmoothingTask
+    {
+        public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
+        {
+            double previousSmoothedY = 0;
+            bool isFirst = true;
+
+            foreach (var dataPoint in data)
+            {
+                double smoothedY;
+                if (isFirst)
+                {
+                    smoothedY = dataPoint.OriginalY;
+                    isFirst = false;
+                }
+                else
+                {
+                    smoothedY = alpha * dataPoint.OriginalY + (1 - alpha) * previousSmoothedY;
+                }
+                yield return new DataPoint(dataPoint.X, dataPoint.OriginalY, smoothedY);
+                previousSmoothedY = smoothedY;
+            }
+        }
+    }
+
+    public class DataPoint
+    {
+        public double X { get; }
+        public double OriginalY { get; }
+        public double SmoothedY { get; } // Переименовано для соответствия тестам.
+
+        public DataPoint(double x, double originalY, double smoothedY)
+        {
+            X = x;
+            OriginalY = originalY;
+            SmoothedY = smoothedY;
+        }
+    }
 }
