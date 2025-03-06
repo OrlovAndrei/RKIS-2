46b05eba8aca23dea1a467bb06f93ef1178a49da
Alex <Alexeychik-uss@mail.ru>
2025-03-06 10:57:28 +0300

    Lesson 1. Updated LimitedSizeStack

--- Занятие 1/LimitedSizeStack/LimitedSizeStack.cs
+++ Занятие 1/LimitedSizeStack/LimitedSizeStack.cs
@@ -11,7 +11,7 @@ public class LimitedSizeStack<T>
     public LimitedSizeStack(int undoLimit)
     {
         if (undoLimit < 0)
-            throw new ArgumentException("Девочки, мы ошиблись.", nameof(undoLimit));
+            throw new ArgumentException("Размер стека не может быть отрицательным.", nameof(undoLimit));
 
         maxSize = undoLimit;
         list = new LinkedList<T>();
@@ -31,7 +31,7 @@ public class LimitedSizeStack<T>
     public T Pop()
     {
         if (list.Count == 0)
-            throw new InvalidOperationException("1000 - 7 ≠ 0");
+            throw new InvalidOperationException("Стек пуст. Невозможно извлечь элемент.");
 
         var first = list.First;
         list.RemoveFirst();
