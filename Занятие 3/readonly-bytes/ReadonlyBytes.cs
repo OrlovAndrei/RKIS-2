cc51d2ad88e4e88cc9095848bc8cef14b8816d1a
Agni <63151335+crash36663@users.noreply.github.com>
2025-03-11 20:15:53 +0300

    Update ReadonlyBytes.cs

--- Занятие 3/readonly-bytes/ReadonlyBytes.cs
+++ Занятие 3/readonly-bytes/ReadonlyBytes.cs
@@ -1,8 +1,60 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace hashes
 {
-	// TODO: Создайте класс ReadonlyBytes
+    public class ReadonlyBytes : IEnumerable<byte>
+    {
+        private readonly byte[] collection;
+        private readonly int hashCode; // Вычисляем hashCode только один раз
+
+        public ReadonlyBytes(params byte[] args)
+        {
+            collection = args ?? throw new ArgumentNullException(nameof(args));
+            unchecked
+            {
+                hashCode = 17;
+                foreach (var b in args)
+                    hashCode = hashCode * 31 + b;
+            }
+        }
+
+        public byte this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= collection.Length) throw new IndexOutOfRangeException();
+                return collection[index];
+            }
+        }
+
+        public int Length => collection.Length;
+
+        public IEnumerator<byte> GetEnumerator()
+        {
+            return ((IEnumerable<byte>)collection).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return collection.SequenceEqual(((ReadonlyBytes)obj).collection);
+        }
+
+        public override int GetHashCode()
+        {
+            return hashCode; // Возвращаем уже вычисленный hashCode
+        }
+
+        public override string ToString()
+        {
+            return $"[{string.Join(", ", collection)}]";
+        }
+    }
 }
