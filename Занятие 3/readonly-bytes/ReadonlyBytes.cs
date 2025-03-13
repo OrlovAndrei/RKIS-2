7c2aa581650f39ed36aa3076042ed92f4534b39e
Павлюк 3834 <konstantinpavluk1@gmail.com>
2025-03-13 15:01:17 +0300

    lesson 3: make ReadonlyBytes

--- Занятие 3/readonly-bytes/ReadonlyBytes.cs
+++ Занятие 3/readonly-bytes/ReadonlyBytes.cs
@@ -1,66 +1,90 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace hashes
 {
-    public class ReadonlyBytes : IEnumerable
+    public class ReadonlyBytes : IReadOnlyList<byte>
     {
-        private byte[] collection;
-        private int hashCode = -1;
+        readonly byte[] array;
+        int hash;
 
-        public ReadonlyBytes(params byte[] args)
+        public ReadonlyBytes(params byte[] array)
         {
-            collection = args ?? throw new ArgumentNullException();
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+            this.array = new byte[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                this.array[i] = array[i];
+            hash = CalculateHashCode();
         }
 
         public byte this[int index]
         {
             get
             {
-                if (index < 0 || index >= collection.Length) throw new IndexOutOfRangeException();
-                return collection[index];
-            }
-        }
-
-        public int Length => collection.Length;
-
-        public IEnumerator<byte> GetEnumerator()
-        {
-            return ((IEnumerable<byte>)collection).GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
-            if (!(obj is ReadonlyBytes comparedBytes) || collection.Length != comparedBytes.Length)
-                return false;
-            return !collection.Where((t, i) => t != comparedBytes[i]).Any();
-        }
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                if (hashCode != -1) return hashCode;
-                hashCode = 1;
-                foreach (var member in collection)
+                try
                 {
-                    hashCode *= 543;
-                    hashCode += member;
+                    return ((IReadOnlyList<byte>)array)[index];
                 }
-                return hashCode;
+                catch (Exception e)
+                {
+                    throw new IndexOutOfRangeException(e.Message, e);
+                }
             }
         }
+
+        int IReadOnlyCollection<byte>.Count => ((IReadOnlyList<byte>)array).Count;
+        public int Length => ((IReadOnlyList<byte>)array).Count;
+
+        bool Equals(ReadonlyBytes other)
+        {
+            if (other == null || Length != other.Length)
+                return false;
+            for (int i = 0; i < Length; i++)
+                if (this[i] != other[i])
+                    return false;
+            return true;
+        }
+
+        public override bool Equals(object other)
+        {
+            if (other == null || GetType() != other.GetType())
+                return false;
+            return this.Equals((ReadonlyBytes)other);
+        }
+
+        int CalculateHashCode()
+        {
+            int hashCode = -985847861;
+            if (array != null)
+                foreach (byte number in array)
+                    hashCode = unchecked(hashCode * -1521134295 + number.GetHashCode());
+            return hashCode;
+        }
+
+        public override int GetHashCode() => hash;
 
         public override string ToString()
         {
-            return $"[{string.Join(", ", collection)}]";
+            string output = "[";
+            if (array.Length > 0)
+            {
+                foreach (byte number in array)
+                    output += number + ", ";
+                output = output.Remove(output.Length - 2);
+            }
+            return output += "]";
+        }
+
+        public IEnumerator<byte> GetEnumerator()
+        {
+            return ((IReadOnlyList<byte>)array).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
