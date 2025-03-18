ef6eb8e50d12b29b664bf9d3b5a4f0edf87eeebb
Izumrudov.Daniil <izumrudov2021@vk.com>
2025-03-19 02:09:57 +0300

    Update ReadonlyBytes.cs

--- Занятие 3/readonly-bytes/ReadonlyBytes.cs
+++ Занятие 3/readonly-bytes/ReadonlyBytes.cs
@@ -4,5 +4,17 @@ using System.Collections.Generic;
 
 namespace hashes
 {
-	// TODO: Создайте класс ReadonlyBytes
-}
+public class ReadonlyBytes : IReadOnlyList<byte>
+    {
+        readonly byte[] array;
+        int hash;
+
+        public ReadonlyBytes(params byte[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+            this.array = new byte[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                this.array[i] = array[i];
+            hash = CalculateHashCode();
+        }
