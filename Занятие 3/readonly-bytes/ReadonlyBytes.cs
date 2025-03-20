87d785e1f76027a1ca8e94ee9748f0a8acaf787c
Roman <karosen1448@gmail.com>
2025-03-20 13:13:13 +0300

    lesson3: make readonly-bytes

--- Занятие 3/readonly-bytes/ReadonlyBytes.cs
+++ Занятие 3/readonly-bytes/ReadonlyBytes.cs
@@ -4,5 +4,44 @@ using System.Collections.Generic;
 
 namespace hashes
 {
-	// TODO: Создайте класс ReadonlyBytes
+	public class ReadonlyBytes : IReadOnlyList<byte>
+    {
+        private readonly byte[] _bytes;
+        private readonly int _hashCode;
+
+        public ReadonlyBytes(byte[] bytes)
+        {
+            if (bytes == null)
+                throw new ArgumentNullException(nameof(bytes));
+            
+            _bytes = bytes.ToArray();
+            _hashCode = ComputeHashCode(_bytes);
+        }
+
+        public int Count => _bytes.Length;
+        public byte this[int index] => _bytes[index];
+
+        public IEnumerator<byte> GetEnumerator() => ((IEnumerable<byte>)_bytes).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => _bytes.GetEnumerator();
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ReadonlyBytes other)
+                return _bytes.SequenceEqual(other._bytes);
+            return false;
+        }
+
+        public override int GetHashCode() => _hashCode;
+
+        private static int ComputeHashCode(byte[] bytes)
+        {
+            unchecked
+            {
+                int hash = 17;
+                foreach (var b in bytes)
+                    hash = hash * 31 + b;
+                return hash;
+            }
+        }
+    }
 }
