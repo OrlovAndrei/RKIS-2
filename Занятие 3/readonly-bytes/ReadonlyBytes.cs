4cb4666980d4aa537b5e1f9d780d22b641956690
Roman <karosen1448@gmail.com>
2025-03-22 11:16:46 +0300

    lesson 3: fix bytes

--- Занятие 3/readonly-bytes/ReadonlyBytes.cs
+++ Занятие 3/readonly-bytes/ReadonlyBytes.cs
@@ -1,47 +1,54 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace hashes
-{
-	public class ReadonlyBytes : IReadOnlyList<byte>
-    {
-        private readonly byte[] _bytes;
-        private readonly int _hashCode;
+namespace hashes {
+    public class ReadonlyBytes: IEnumerable < byte > {
+        private readonly byte[] byteArray;
+        private int ? cachedHashCode;
 
-        public ReadonlyBytes(byte[] bytes)
-        {
-            if (bytes == null)
+        public ReadonlyBytes(params byte[] bytes) {
+            byteArray = bytes ??
                 throw new ArgumentNullException(nameof(bytes));
-            
-            _bytes = bytes.ToArray();
-            _hashCode = ComputeHashCode(_bytes);
         }
 
-        public int Count => _bytes.Length;
-        public byte this[int index] => _bytes[index];
-
-        public IEnumerator<byte> GetEnumerator() => ((IEnumerable<byte>)_bytes).GetEnumerator();
-        IEnumerator IEnumerable.GetEnumerator() => _bytes.GetEnumerator();
-
-        public override bool Equals(object obj)
-        {
-            if (obj is ReadonlyBytes other)
-                return _bytes.SequenceEqual(other._bytes);
-            return false;
-        }
-
-        public override int GetHashCode() => _hashCode;
-
-        private static int ComputeHashCode(byte[] bytes)
-        {
-            unchecked
-            {
-                int hash = 17;
-                foreach (var b in bytes)
-                    hash = hash * 31 + b;
-                return hash;
+        public byte this[int index] {
+            get {
+                if (index < 0 || index >= byteArray.Length)
+                    throw new IndexOutOfRangeException(nameof(index));
+                return byteArray[index];
             }
         }
+
+        public int Length => byteArray.Length;
+
+        public IEnumerator < byte > GetEnumerator() => ((IEnumerable < byte > ) byteArray).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public override bool Equals(object obj) {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            var otherBytes = (ReadonlyBytes) obj;
+            return Length == otherBytes.Length && byteArray.SequenceEqual(otherBytes.byteArray);
+        }
+
+        public override int GetHashCode() {
+            if (!cachedHashCode.HasValue) {
+                unchecked {
+                    cachedHashCode = 1;
+                    foreach(var byteValue in byteArray) {
+                        cachedHashCode = cachedHashCode.Value * 543 + byteValue;
+                    }
+                }
+            }
+            return cachedHashCode.Value;
+        }
+
+        public override string ToString() => $ "[{string.Join(",", byteArray)}]";
     }
 }
