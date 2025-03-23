91ab8ff63675e422d00b312bccec2f8934b758f1
BlackPawn <dima.zhidkov00@gmail.com>
2025-03-23 11:03:08 +0300

    lesson 4 : make 2

--- Занятие 4/Filters/ProductService.cs
+++ Занятие 4/Filters/ProductService.cs
@@ -15,6 +15,53 @@
 			return filteredProducts;
 		}
 
-		//напишите методы Not, And, Or и Xor
+		// Метод Not: инвертирует результат фильтра
+        public static Func<Product, bool> Not(Func<Product, bool> filter)
+        {
+            return product => !filter(product);
+        }
+
+        // Метод And: возвращает true, если все фильтры возвращают true
+        public static Func<Product, bool> And(params Func<Product, bool>[] filters)
+        {
+            return product =>
+            {
+                foreach (var filter in filters)
+                {
+                    if (!filter(product))
+                        return false;
+                }
+                return true;
+            };
+        }
+
+        // Метод Or: возвращает true, если хотя бы один фильтр возвращает true
+        public static Func<Product, bool> Or(params Func<Product, bool>[] filters)
+        {
+            return product =>
+            {
+                foreach (var filter in filters)
+                {
+                    if (filter(product))
+                        return true;
+                }
+                return false;
+            };
+        }
+
+        // Метод Xor: возвращает true, если только один фильтр возвращает true
+        public static Func<Product, bool> Xor(params Func<Product, bool>[] filters)
+        {
+            return product =>
+            {
+                int trueCount = 0;
+                foreach (var filter in filters)
+                {
+                    if (filter(product))
+                        trueCount++;
+                }
+                return trueCount == 1;
+            };
+        }
 	}
 }
