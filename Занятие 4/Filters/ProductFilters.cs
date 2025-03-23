35072b5c23f7c76d0ee8614928ef2720374e5b52
BlackPawn <dima.zhidkov00@gmail.com>
2025-03-23 11:01:59 +0300

    lesson 4 : make 1

--- Занятие 4/Filters/ProductFilters.cs
+++ Занятие 4/Filters/ProductFilters.cs
@@ -4,27 +4,38 @@
 	{
 		public static Func<Product, bool> FilterByCategory(Category category)
 		{
-			return ...;
+			return product => product.Category == category;
 		}
 
 		public static Func<Product, bool> FilterByPrice(decimal minPrice, decimal maxPrice)
 		{
-			return ...;
+			return product => product.Price >= minPrice && product.Price <= maxPrice;
 		}
 
 		public static Func<Product, bool> FilterByStock(int minStock)
 		{
-			return ...;
+			return product => product.StockCount >= minStock;
 		}
 
 		public static Func<Product, bool> FilterByExpirationDate(DateTime date)
 		{
-			return ...
+			return product =>
+    		{
+       	 		// Если срок годности не указан, товар подходит
+        		if (product.ExpirationDate == null)
+            	return true;
+
+        		// Вычисляем дату истечения срока годности
+       			 DateTime expirationDate = product.ProductionDate + product.ExpirationDate.Value;
+
+        		// Проверяем, что срок годности не истек
+        		return expirationDate >= date;
+    		};
 		}
 
 		public static Func<Product, bool> FilterByNameContains(string namePart)
 		{
-			return ...;
+			return product => product.Name.Contains(namePart, StringComparison.OrdinalIgnoreCase);
 		}
 	}
 }
