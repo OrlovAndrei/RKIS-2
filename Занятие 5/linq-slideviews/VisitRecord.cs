0019fdbee9bf04b384f54b9fa7b299d58324ef0e
Евгений Якушев <akusev1107@gmail.com>
2025-04-07 12:12:05 +0300

    make Lesson 5

--- Занятие 5/linq-slideviews/VisitRecord.cs
+++ Занятие 5/linq-slideviews/VisitRecord.cs
@@ -1,21 +1,51 @@
 ﻿using System;
 
-namespace linq_slideviews;
-
-public record VisitRecord(int UserId, int SlideId, DateTime DateTime, SlideType SlideType)
+namespace linq_slideviews
 {
-	public override string ToString() =>
-		$"{nameof(UserId)}: {UserId}, {nameof(SlideId)}: {SlideId}, {nameof(SlideType)}: {SlideType}, {nameof(DateTime)}: {DateTime}";
+    public class VisitRecord
+    {
+        public VisitRecord(int userId, int slideId, DateTime dateTime, SlideType slideType)
+        {
+            SlideType = slideType;
+            UserId = userId;
+            SlideId = slideId;
+            DateTime = dateTime;
+        }
 
-	public override int GetHashCode()
-	{
-		unchecked
-		{
-			var hashCode = UserId;
-			hashCode = (hashCode * 397) ^ SlideId;
-			hashCode = (hashCode * 397) ^ (int)SlideType;
-			hashCode = (hashCode * 397) ^ DateTime.GetHashCode();
-			return hashCode;
-		}
-	}
+        public int UserId;
+        public int SlideId;
+        public SlideType SlideType;
+        public DateTime DateTime;
+
+        public override string ToString()
+        {
+            return $"{nameof(UserId)}: {UserId}, {nameof(SlideId)}: {SlideId}, {nameof(SlideType)}: {SlideType}, {nameof(DateTime)}: {DateTime}";
+        }
+
+        protected bool Equals(VisitRecord other)
+        {
+            return UserId == other.UserId && SlideId == other.SlideId && SlideType == other.SlideType && DateTime.Equals(other.DateTime);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((VisitRecord)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = UserId;
+                hashCode = (hashCode * 397) ^ SlideId;
+                hashCode = (hashCode * 397) ^ (int)SlideType;
+                hashCode = (hashCode * 397) ^ DateTime.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+
 }
