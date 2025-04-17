f1db6071146c29ec5f04573c01bc9d33f5417483
Git-art-dark <vladimirrumancev27544@gmail.com>
2025-04-17 15:54:00 +0300

    lesson 6: make Bank

--- Занятие 6/Bank/BankAccount.cs
+++ Занятие 6/Bank/BankAccount.cs
@@ -3,23 +3,49 @@ using System.Threading.Tasks;
 
 namespace Bank
 {
-	public class BankAccount
-	{
-		public Guid Id { get; } = Guid.NewGuid();
+    public class BankAccount
+    {
+        private decimal balance;
 
-		public decimal GetBalance()
-		{
-			throw new NotImplementedException();
-		}
+        public Guid Id { get; } = Guid.NewGuid();
 
-		public async Task DepositAsync(decimal amount)
-		{
-			throw new NotImplementedException();
-		}
+        // Метод для получения текущего баланса
+        public decimal GetBalance()
+        {
+            return balance;
+        }
 
-		public async Task WithdrawAsync(decimal amount)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        // Метод для пополнения счета
+        public async Task DepositAsync(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Сумма должна быть положительной", nameof(amount));
+            }
+
+            // Имитация долгих вычислений
+            await Task.Delay(100);
+
+            balance += amount;
+        }
+
+        // Метод для снятия средств
+        public async Task WithdrawAsync(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Сумма должна быть положительной", nameof(amount));
+            }
+
+            if (balance < amount)
+            {
+                throw new InvalidOperationException("Недостаточно средств");
+            }
+
+            // Имитация долгих вычислений
+            await Task.Delay(100);
+
+            balance -= amount;
+        }
+    }
 }
--- Занятие 6/Bank/BankServer.cs
+++ Занятие 6/Bank/BankServer.cs
@@ -1,27 +1,60 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Bank
 {
-	public class BankServer
-	{
-		private readonly ConcurrentDictionary<Guid, BankAccount> _accounts = new();
+    public class BankServer
+    {
+        private readonly ConcurrentDictionary<Guid, BankAccount> _accounts = new();
 
-		public async Task<Guid> CreateAccount(decimal initialBalance = 0)
-		{
-			throw new NotImplementedException();
-		}
+        // Создание нового счета с начальным балансом
+        public async Task<Guid> CreateAccount(decimal initialBalance = 0)
+        {
+            if (initialBalance < 0)
+            {
+                throw new ArgumentException("Начальный баланс не может быть отрицательным", nameof(initialBalance));
+            }
 
-		public async Task PerformTransactionAsync(Guid fromAccountId, Guid toAccountId, decimal amount)
-		{
-			throw new NotImplementedException();
-		}
+            var account = new BankAccount();
+            await account.DepositAsync(initialBalance);
+            _accounts[account.Id] = account;
+            return account.Id;
+        }
 
-		public async Task<decimal> GetAccountBalanceAsync(Guid accountId)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        // Выполнение транзакции (перевод средств между счетами)
+        public async Task PerformTransactionAsync(Guid fromAccountId, Guid toAccountId, decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Сумма должна быть положительной", nameof(amount));
+            }
+
+            if (!_accounts.ContainsKey(fromAccountId) || !_accounts.ContainsKey(toAccountId))
+            {
+                throw new KeyNotFoundException("Один или оба идентификатора учетной записи недействительны");
+            }
+
+            var fromAccount = _accounts[fromAccountId];
+            var toAccount = _accounts[toAccountId];
+
+            // Имитация перевода средств с задержкой для асинхронности
+            await Task.WhenAll(
+                fromAccount.WithdrawAsync(amount),
+                toAccount.DepositAsync(amount)
+            );
+        }
+
+        // Получение баланса счета
+        public async Task<decimal> GetAccountBalanceAsync(Guid accountId)
+        {
+            if (!_accounts.ContainsKey(accountId))
+            {
+                throw new KeyNotFoundException("Account not found");
+            }
+
+            var account = _accounts[accountId];
+            return account.GetBalance();
+        }
+    }
 }
