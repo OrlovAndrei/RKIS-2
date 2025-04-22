b6695fcbefbed6ff2b38b06518f469a1146dd38c
Евгений Якушев <akusev1107@gmail.com>
2025-04-22 17:26:22 +0300

    Lesson 7: make task

--- Занятие 7/cripto-reader/Program.cs
+++ Занятие 7/cripto-reader/Program.cs
@@ -3,15 +3,14 @@ using cripto_reader.UI;
 
 namespace cripto_reader
 {
-	internal class Program
-	{
-		[STAThread]
-		public static void Main(string[] args) => BuildAvaloniaApp()
-			.StartWithClassicDesktopLifetime(args);
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
 
-		public static AppBuilder BuildAvaloniaApp()
-			=> AppBuilder.Configure<App>()
-				.UsePlatformDetect();
-	}
-}
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect();
+    }
 }
