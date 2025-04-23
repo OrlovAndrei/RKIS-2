030a6de339af6b7ed467ff8e1815715536435088
darkarat <dar.karat3011@gmail.com>
2025-04-23 23:25:07 +0300

    lesson 7: make task 1

--- Занятие 7/cripto-reader/AesCrypt.cs
+++ Занятие 7/cripto-reader/AesCrypt.cs
@@ -1,15 +1,44 @@
-﻿namespace cripto_reader
-{
-	static class AesCrypt
-	{
-		public static byte[] Encrypt(string plainText, string password)
-		{
-			throw new NotImplementedException();
-		}
+﻿using System.Security.Cryptography;
+using System.Text;
 
-		public static string Decrypt(byte[] cipherText, string password)
-		{
-			throw new NotImplementedException();
-		}
-	}
+namespace cripto_reader
+{
+    static class AesCrypt
+    {
+        public static byte[] Encrypt(string plainText, string password)
+        {
+            if (plainText is null)
+                throw new ArgumentNullException(nameof(plainText));
+            if (password is null)
+                throw new ArgumentNullException(nameof(password));
+
+            using var aes = Aes.Create();
+            aes.Key = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+            aes.IV = MD5.HashData(Encoding.UTF8.GetBytes(password));
+
+            using var ms = new MemoryStream();
+            using (var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                cs.Write(Encoding.UTF8.GetBytes(plainText));
+            
+            return ms.ToArray();
+        }
+
+        public static string Decrypt(byte[] cipherText, string password)
+        {
+            if (cipherText is null)
+                throw new ArgumentNullException(nameof(cipherText));
+            if (password is null)
+                throw new ArgumentNullException(nameof(password));
+
+            using var aes = Aes.Create();
+            aes.Key = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+            aes.IV = MD5.HashData(Encoding.UTF8.GetBytes(password));
+
+            using var ms = new MemoryStream(cipherText);
+            using var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            using var sr = new StreamReader(cs);
+            
+            return sr.ReadToEnd();
+        }
+    }
 }
