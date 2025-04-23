655713e270aa46ea60840a4803562ff801ae9c3e
maxiscod <164271293+maxiscod@users.noreply.github.com>
2025-04-23 23:26:22 +0300

    Update Program.cs

--- Занятие 7/cripto-reader/Program.cs
+++ Занятие 7/cripto-reader/Program.cs
@@ -3,13 +3,12 @@ using cripto_reader.UI;
 
 namespace cripto_reader
 {
+	public class Program
 	internal class Program
 	{
 		[STAThread]
 		public static void Main(string[] args) => BuildAvaloniaApp()
-			.StartWithClassicDesktopLifetime(args);
-
-		public static AppBuilder BuildAvaloniaApp()
+@@ -13,4 +13,4 @@ public static AppBuilder BuildAvaloniaApp()
 			=> AppBuilder.Configure<App>()
 				.UsePlatformDetect();
 	}
