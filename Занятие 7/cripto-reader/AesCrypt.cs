c0f7408822476e9ada15092f211954fa2cd6aa78
GOL <164672521+accum104@users.noreply.github.com>
2025-04-24 20:09:13 +0300

    Shcherbinin. lesson 7 AesCrypt.cs

--- Занятие 7/cripto-reader/AesCrypt.cs
+++ Занятие 7/cripto-reader/AesCrypt.cs
@@ -1,15 +1,60 @@
-﻿namespace cripto_reader
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace cripto_reader
 {
 	static class AesCrypt
 	{
+		// Генерация ключа и IV на основе пароля
+		private static void GenerateKeyAndIV(string password, out byte[] key, out byte[] iv)
+		{
+			using (SHA256 sha256 = SHA256.Create())
+			using (MD5 md5 = MD5.Create())
+			{
+				key = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+				iv = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+			}
+		}
+
 		public static byte[] Encrypt(string plainText, string password)
 		{
-			throw new NotImplementedException();
+			GenerateKeyAndIV(password, out byte[] key, out byte[] iv);
+
+			using (Aes aes = Aes.Create())
+			{
+				aes.Key = key;
+				aes.IV = iv;
+
+				using (MemoryStream ms = new MemoryStream())
+				using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+				using (StreamWriter sw = new StreamWriter(cs, Encoding.UTF8))
+				{
+					sw.Write(plainText);
+					sw.Flush();
+					cs.FlushFinalBlock();
+					return ms.ToArray();
+				}
+			}
 		}
 
 		public static string Decrypt(byte[] cipherText, string password)
 		{
-			throw new NotImplementedException();
+			GenerateKeyAndIV(password, out byte[] key, out byte[] iv);
+
+			using (Aes aes = Aes.Create())
+			{
+				aes.Key = key;
+				aes.IV = iv;
+
+				using (MemoryStream ms = new MemoryStream(cipherText))
+				using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
+				using (StreamReader sr = new StreamReader(cs, Encoding.UTF8))
+				{
+					return sr.ReadToEnd();
+				}
+			}
 		}
 	}
 }
