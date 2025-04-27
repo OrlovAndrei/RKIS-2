380b47b91f61f2f95878949df2fe3a13660ea3b4
BlackPawn <dima.zhidkov00@gmail.com>
2025-04-27 21:05:55 +0300

    lesson 8 : make 1

--- Занятие 8/chik-chirik/Models/Post.cs
+++ Занятие 8/chik-chirik/Models/Post.cs
@@ -2,5 +2,10 @@
 {
 	public class Post
 	{
-	}
+	public int Id { get; set; }          // ID поста
+        public int UserId { get; set; }      // ID автора
+        public string Title { get; set; }    // Заголовок поста
+        public string Body { get; set; }     // Текст поста
+        public User User { get; set; }       // Ссылка на автора (будет заполняться отдельно)
+    }
 }
