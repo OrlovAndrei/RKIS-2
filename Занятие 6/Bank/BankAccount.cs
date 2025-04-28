e5fe965695996f55105daa940337e7f96279fd2e
savitar696 <karosen1448@gmail.com>
2025-04-28 03:19:04 +0300

    lesson 6: make BankService
    Удалил полностью репозиторий, вышло по ошибке

--- Занятие 6/Bank/BankAccount.cs
+++ Занятие 6/Bank/BankAccount.cs
@@ -6,20 +6,44 @@ namespace Bank
 	public class BankAccount
 	{
 		public Guid Id { get; } = Guid.NewGuid();
+		private decimal _balance = 0;
+		private readonly object _balanceLock = new object();
 
 		public decimal GetBalance()
 		{
-			throw new NotImplementedException();
+			lock (_balanceLock)
+			{
+				return _balance;
+			}
 		}
 
 		public async Task DepositAsync(decimal amount)
 		{
-			throw new NotImplementedException();
+			if (amount < 0)
+				throw new ArgumentException("Сумма пополнения не может быть отрицательной", nameof(amount));
+				
+			await Task.Delay(100);
+			
+			lock (_balanceLock)
+			{
+				_balance += amount;
+			}
 		}
 
 		public async Task WithdrawAsync(decimal amount)
 		{
-			throw new NotImplementedException();
+			if (amount < 0)
+				throw new ArgumentException("Сумма снятия не может быть отрицательной", nameof(amount));
+				
+			await Task.Delay(100);
+			
+			lock (_balanceLock)
+			{
+				if (_balance < amount)
+					throw new InvalidOperationException("Недостаточно средств на счете");
+					
+				_balance -= amount;
+			}
 		}
 	}
 }
