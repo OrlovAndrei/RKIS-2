41201e3466e85d15cc67571eefa7a8bca60525a4
savitar696 <karosen1448@gmail.com>
2025-04-28 03:13:38 +0300

    lesson 7: make crypto

--- Занятие 7/cripto-reader/AesCrypt.cs
+++ Занятие 7/cripto-reader/AesCrypt.cs
@@ -1,15 +1,65 @@
-﻿namespace cripto_reader
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace cripto_reader
 {
 	static class AesCrypt
 	{
 		public static byte[] Encrypt(string plainText, string password)
 		{
-			throw new NotImplementedException();
+			if (string.IsNullOrEmpty(plainText))
+				return Encoding.UTF8.GetBytes(string.Empty);
+
+			byte[] key = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+			byte[] iv = MD5.HashData(Encoding.UTF8.GetBytes(password));
+
+			using (Aes aes = Aes.Create())
+			{
+				aes.Key = key;
+				aes.IV = iv;
+
+				using (MemoryStream memoryStream = new MemoryStream())
+				{
+					using (CryptoStream cryptoStream = new CryptoStream(
+						memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+					{
+						using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+						{
+							streamWriter.Write(plainText);
+						}
+					}
+					return memoryStream.ToArray();
+				}
+			}
 		}
 
 		public static string Decrypt(byte[] cipherText, string password)
 		{
-			throw new NotImplementedException();
+			if (cipherText == null || cipherText.Length == 0)
+				return string.Empty;
+
+			byte[] key = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+			byte[] iv = MD5.HashData(Encoding.UTF8.GetBytes(password));
+
+			using (Aes aes = Aes.Create())
+			{
+				aes.Key = key;
+				aes.IV = iv;
+
+				using (MemoryStream memoryStream = new MemoryStream(cipherText))
+				{
+					using (CryptoStream cryptoStream = new CryptoStream(
+						memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Read))
+					{
+						using (StreamReader streamReader = new StreamReader(cryptoStream))
+						{
+							return streamReader.ReadToEnd();
+						}
+					}
+				}
+			}
 		}
 	}
 }
