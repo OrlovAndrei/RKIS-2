727bd052d8af1d6bc2b75dd3e7e7bd0e343b635d
BlackPawn <dima.zhidkov00@gmail.com>
2025-05-04 11:16:23 +0300

    lesson 9 : make 1

--- Занятие 9/TodoList/TodoItem.cs
+++ Занятие 9/TodoList/TodoItem.cs
@@ -3,9 +3,9 @@
 	public class TodoItem
 	{
 		public Guid Id { get; } = Guid.NewGuid();
-		public string Text { get; set; } = "";
-		public DateTime StartTime { get; } = DateTime.Now;
-		public DateTime? EndTime { get; set; 
-		public bool IsCompleted { get; set; }
+        public string Text { get; set; } = "";
+        public DateTime StartTime { get; } = DateTime.Now;
+        public DateTime? EndTime { get; set; }
+        public bool IsCompleted { get; set; }
 	}
 }
--- Занятие 9/TodoList/TodoService.cs
+++ Занятие 9/TodoList/TodoService.cs
@@ -4,29 +4,52 @@ namespace TodoList
 {
 	public class TodoService
 	{
+		private readonly AppDbContext _dbContext;
+
+        public TodoService()
+        {
+            _dbContext = new AppDbContext();
+            _dbContext.Database.EnsureCreated();
+        }
 		public async Task<TodoItem> AddTodo(TodoItem item)
 		{
-			throw new NotImplementedException();
+			_dbContext.TodoItems.Add(item);
+            await _dbContext.SaveChangesAsync();
+            return item;
 		}
 
 		public async Task<TodoItem>? UpdateTodo(TodoItem item)
 		{
-			throw new NotImplementedException();
+			var existingItem = await _dbContext.TodoItems.FindAsync(item.Id);
+            if (existingItem == null)
+                return null;
+
+            existingItem.Text = item.Text;
+            existingItem.IsCompleted = item.IsCompleted;
+            existingItem.EndTime = item.EndTime;
+
+            await _dbContext.SaveChangesAsync();
+            return existingItem;
 		}
 
 		public async Task DeleteTodo(Guid id)
 		{
-			throw new NotImplementedException();
+			var item = await _dbContext.TodoItems.FindAsync(id);
+            if (item != null)
+            {
+                _dbContext.TodoItems.Remove(item);
+                await _dbContext.SaveChangesAsync();
+            }
 		}
 
 		public async Task<List<TodoItem>> GetAllTodos()
 		{
-			throw new NotImplementedException();
+			return await _dbContext.TodoItems.ToListAsync();
 		}
 
 		public async Task<TodoItem>? GetByIdTodos(Guid id)
 		{
-			throw new NotImplementedException();
+			return await _dbContext.TodoItems.FindAsync(id);
 		}
 	}
 }
