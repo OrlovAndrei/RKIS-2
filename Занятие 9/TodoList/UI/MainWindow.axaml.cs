e20aa3f66ca41050181d8a64fc1bf381169e960e
RUDxNIK <wwwnikrud1205@mail.ru>
2025-05-08 00:53:29 +0300

    lesson 9: make TodoList

--- Занятие 9/TodoList/UI/MainWindow.axaml.cs
+++ Занятие 9/TodoList/UI/MainWindow.axaml.cs
@@ -4,80 +4,83 @@ using System.Collections.ObjectModel;
 
 namespace TodoList.UI
 {
-	public class TodoItemWithEditing
-	{
-		public TodoItem Todo { get; set; }
-		public bool IsEditing { get; set; }
-	}
+    public class TodoItemWithEditing
+    {
+        public TodoItem Todo { get; set; }
+        public bool IsEditing { get; set; }
+    }
 
-	public partial class MainWindow : Window
-	{
-		private readonly TodoService _todoService = new();
-		private ObservableCollection<TodoItemWithEditing> _todos = new();
+    public partial class MainWindow : Window
+    {
+        private readonly TodoService _todoService = new();
+        private ObservableCollection<TodoItemWithEditing> _todos = new();
 
-		public MainWindow()
-		{
-			InitializeComponent();
-			TodosItemsControl.ItemsSource = _todos;
-			LoadTodos();
-		}
+        public MainWindow()
+        {
+            InitializeComponent();
+            TodosItemsControl.ItemsSource = _todos;
+            LoadTodos();
+        }
 
-		private async void LoadTodos()
-		{
-			var todos = await _todoService.GetAllTodos();
-			var todoItemWithEditing = todos.Select(t =>
-				new TodoItemWithEditing() { Todo = t, IsEditing = false });
-			_todos = new ObservableCollection<TodoItemWithEditing>(todoItemWithEditing);
-			TodosItemsControl.ItemsSource = _todos;
-		}
+        private async void LoadTodos()
+        {
+            var todos = await _todoService.GetAllTodos();
+            var todoItemWithEditing = todos.Select(t =>
+                new TodoItemWithEditing() { Todo = t, IsEditing = false });
+            _todos = new ObservableCollection<TodoItemWithEditing>(todoItemWithEditing);
+            TodosItemsControl.ItemsSource = _todos;
+        }
 
-		private async void AddTodo_Click(object sender, RoutedEventArgs e)
-		{
-			if (string.IsNullOrWhiteSpace(NewTodoTextBox.Text)) return;
+        private async void AddTodo_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(NewTodoTextBox.Text)) return;
 
-			var newItem = new TodoItem { Text = NewTodoTextBox.Text };
-			await _todoService.AddTodo(newItem);
-			_todos.Add(new TodoItemWithEditing() { Todo = newItem});
-			NewTodoTextBox.Text = "";
-		}
+            var newItem = new TodoItem { Text = NewTodoTextBox.Text };
+            await _todoService.AddTodo(newItem);
+            _todos.Add(new TodoItemWithEditing() { Todo = newItem });
+            NewTodoTextBox.Text = "";
+        }
 
-		private async void DeleteTodo_Click(object sender, RoutedEventArgs e)
-		{
-			if (sender is Button btn && btn.DataContext is TodoItemWithEditing item)
-			{
-				await _todoService.DeleteTodo(item.Todo.Id);
-				_todos.Remove(item);
-			}
-		}
+        private async void DeleteTodo_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.DataContext is TodoItemWithEditing item)
+            {
+                await _todoService.DeleteTodo(item.Todo.Id);
+                _todos.Remove(item);
+            }
+        }
 
-		private async void EditSave_Click(object sender, RoutedEventArgs e)
-		{
-			if (sender is Button btn && btn.DataContext is TodoItemWithEditing item)
-			{
-				if (item.IsEditing)
-					await _todoService.UpdateTodo(item.Todo);
+        private async void EditSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.DataContext is TodoItemWithEditing item)
+            {
+                if (item.IsEditing)
+                {
+                    // Используем UpdateTodoText вместо UpdateTodo
+                    await _todoService.UpdateTodoText(item.Todo.Id, item.Todo.Text);
+                }
 
-				item.IsEditing = !item.IsEditing;
-				RefreshItem(item);
-			}
-		}
+                item.IsEditing = !item.IsEditing;
+                RefreshItem(item);
+            }
+        }
 
-		private async void CheckBox_Click(object sender, RoutedEventArgs e)
-		{
-			if (sender is CheckBox checkBox && checkBox.DataContext is TodoItemWithEditing item)
-			{
-				item.Todo.IsCompleted = checkBox.IsChecked ?? false;
-				item.Todo.EndTime = item.Todo.IsCompleted ? DateTime.Now : null;
-				await _todoService.UpdateTodo(item.Todo);
-				RefreshItem(item);
-			}
-		}
+        private async void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is CheckBox checkBox && checkBox.DataContext is TodoItemWithEditing item)
+            {
+                item.Todo.IsCompleted = checkBox.IsChecked ?? false;
+                item.Todo.EndTime = item.Todo.IsCompleted ? DateTime.Now : null;
+                // Используем ToggleTodoCompletion вместо UpdateTodo
+                await _todoService.ToggleTodoCompletion(item.Todo);
+                RefreshItem(item);
+            }
+        }
 
-		private void RefreshItem(TodoItemWithEditing item)
-		{
-			// Принудительное обновление элемента
-			var index = _todos.IndexOf(item);
-			_todos[index] = item;
-		}
-	}
+        private void RefreshItem(TodoItemWithEditing item)
+        {
+            var index = _todos.IndexOf(item);
+            _todos[index] = item;
+        }
+    }
 }
