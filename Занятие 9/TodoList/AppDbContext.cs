6d963eb2031f0bbbe32564148e2c56677a8d3d41
Ремизов Дмитрий 3834 <remizovdmitrij8@gmail.com>
2025-05-07 20:56:21 +0300

    Lesson 9 : make Update AppDbContext.cs

--- Занятие 9/TodoList/AppDbContext.cs
+++ Занятие 9/TodoList/AppDbContext.cs
@@ -2,7 +2,20 @@
 
 namespace TodoList
 {
-	public class AppDbContext : DbContext
-	{
-	}
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=todolist.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>()
+                .Property(t => t.Id)
+                .ValueGeneratedOnAdd();
+        }
+    }
 }
