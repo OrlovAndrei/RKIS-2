293e555c5153caca017641d851870d8c4606dadc
Alex <Alexeychik-uss@mail.ru>
2025-05-08 19:10:29 +0300

    Lesson 9. TodoList

--- Занятие 9/TodoList/TodoItem.cs
+++ Занятие 9/TodoList/TodoItem.cs
@@ -5,7 +5,7 @@
 		public Guid Id { get; } = Guid.NewGuid();
 		public string Text { get; set; } = "";
 		public DateTime StartTime { get; } = DateTime.Now;
-		public DateTime? EndTime { get; set; 
+		public DateTime? EndTime { get; set; }
 		public bool IsCompleted { get; set; }
 	}
 }
--- Занятие 9/TodoList/TodoService.cs
+++ Занятие 9/TodoList/TodoService.cs
@@ -1,32 +1,47 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace TodoList
 {
-	public class TodoService
-	{
-		public async Task<TodoItem> AddTodo(TodoItem item)
-		{
-			throw new NotImplementedException();
-		}
+    public class TodoService
+    {
+        private readonly List<TodoItem> _todos = new();
 
-		public async Task<TodoItem>? UpdateTodo(TodoItem item)
-		{
-			throw new NotImplementedException();
-		}
+        public Task<TodoItem> AddTodo(TodoItem item)
+        {
+            _todos.Add(item);
+            return Task.FromResult(item);
+        }
 
-		public async Task DeleteTodo(Guid id)
-		{
-			throw new NotImplementedException();
-		}
+        public Task<TodoItem?> UpdateTodo(TodoItem item) // Изменили возвращаемый тип на TodoItem?
+        {
+            var existing = _todos.FirstOrDefault(t => t.Id == item.Id);
+            if (existing != null)
+            {
+                existing.Text = item.Text;
+                existing.IsCompleted = item.IsCompleted;
+                existing.EndTime = item.EndTime;
+                return Task.FromResult<TodoItem?>(existing);
+            }
+            return Task.FromResult<TodoItem?>(null); // Явно указываем тип возвращаемого null
+        }
 
-		public async Task<List<TodoItem>> GetAllTodos()
-		{
-			throw new NotImplementedException();
-		}
+        public Task DeleteTodo(Guid id)
+        {
+            _todos.RemoveAll(t => t.Id == id);
+            return Task.CompletedTask;
+        }
 
-		public async Task<TodoItem>? GetByIdTodos(Guid id)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public Task<List<TodoItem>> GetAllTodos()
+        {
+            return Task.FromResult(_todos.ToList());
+        }
+
+        public Task<TodoItem?> GetByIdTodos(Guid id) // Изменили возвращаемый тип на TodoItem?
+        {
+            return Task.FromResult(_todos.FirstOrDefault(t => t.Id == id));
+        }
+    }
 }
