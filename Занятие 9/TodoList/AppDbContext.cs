f338a38196bbcd0d5568dec09afbcf53b3aaf291
hatsereon <hatsereon@DREAM>
2025-05-08 16:41:08 +0300

    Lesson 9: Done

--- Занятие 9/TodoList/AppDbContext.cs
+++ Занятие 9/TodoList/AppDbContext.cs
@@ -2,7 +2,18 @@
 
 namespace TodoList
 {
-	public class AppDbContext : DbContext
-	{
-	}
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>()
+                .HasKey(t => t.Id); 
+        }
+    }
 }
